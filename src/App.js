41a16c923f5a2c678bed8d205ebfd17e321b86a5
Shashank Mittal <shashankmittal@Shashanks-MacBook-Pro.local>
2024-08-08 21:05:50 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className='text-3xl font-bold underline text-cyan-800'>Hello world!</h1>
+  );
+}
+
+export default App;
